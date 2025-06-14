abf2d3bbb9359d82ea6923e1d2eff0b3574098ef
LahiruMadushann <lahirumadushandl@gmail.com>
2025-06-14 16:23:32 +0530

    feat: implement header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>
+        Weather Reporter
+      </h1>
+      <p>Real time weather information</p>
+    </div>
+  )
+}
+
+export default Header
